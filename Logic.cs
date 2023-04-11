0063889e3c5af9f5a3b1d7acf744ac2d4ab0e426
j <j@LEMON>
2023-04-11 21:51:16 +0000

    Add project files.

---
+++ Logic.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public class Logic
+    {
+        public string CurrentPlayer { get; set; } = X;
+        private const string X = "X";
+        private const string O = "O";
+        private string[,] Board = new string[3,3];
+        
+        public void SetNextPlayer()
+        {
+            if (CurrentPlayer == X)
+            {
+                CurrentPlayer = O;
+            }
+            else
+            {
+                CurrentPlayer = X;
+            }
+        }
+        public bool PlayerWin()
+        {
+            //check row            
+            for(var i=0;i<3;i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[i, 0]))
+                {
+                    if (Board[i,0] == Board[i,1] && Board[i,0] == Board[i, 2])
+                    {
+                        return true;
+                    }   
+                }
+            } 
+            //Column
+            for (var i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[0, i]))
+                {
+                    if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            //diagonal
+
+                if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+                {
+                    if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+                    {
+                        return true;
+                    }
+                    if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+                    {
+                        return true;
+                    }
+                }
+            
+            return false;
+        }
+        public bool PlayerDraw()
+        {
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    if (String.IsNullOrWhiteSpace(Board[i, j]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public void UpdateBoard(Position position, string value) 
+        {
+            Board[position.x,position.y] = value;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = winCount;
+        }
+        Logic _Logic = new Logic();
+        WinCount winCount = new WinCount(){};
+        public class WinCount : INotifyPropertyChanged
+        {
+            private int playerXCount = 0;
+            public int PlayerXCount
+            {
+                get { return playerXCount; }
+                set
+                {
+                    playerXCount = value;
+                    OnPropertyChanged(nameof(PlayerXCount));
+                }
+            }
+
+            private int playerOCount = 0;
+            public int PlayerOCount
+            {
+                get { return playerOCount; }
+                set
+                { 
+                    playerOCount = value;
+                    OnPropertyChanged(nameof(PlayerOCount));
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private void PlayerClickSpace(object sender, RoutedEventArgs e)
+        {
+            var space = (Button)sender;
+            if(!String.IsNullOrWhiteSpace(space.Content?.ToString())) return;
+            space.Content = _Logic.CurrentPlayer;
+
+            var coodinates = space.Tag?.ToString().Split(',');
+            var xValue = int.Parse(coodinates[0]);
+            var yValue = int.Parse(coodinates[1]);
+
+
+            var buttonPosition = new Position() { x = xValue, y = yValue }; 
+            _Logic.UpdateBoard(buttonPosition,_Logic.CurrentPlayer);
+            if (_Logic.PlayerWin())
+            {
+                MessageBox.Show($"PLAYER {_Logic.CurrentPlayer} WINS!!!");
+                if (_Logic.CurrentPlayer == "X")
+                {
+                    winCount.PlayerXCount++;
+                }
+                else
+                {
+                    winCount.PlayerOCount++;
+                }
+                NewGame();
+                _Logic.SetNextPlayer();
+
+
+            }
+            else if(_Logic.PlayerDraw()) 
+            {
+                MessageBox.Show("DRAW");
+                NewGame();
+                _Logic.SetNextPlayer();
+            }
+            _Logic.SetNextPlayer();
+
+
+        }
+        public void btnResetClick(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+            winCount.PlayerXCount = 0;
+            winCount.PlayerOCount = 0;
+        }
+        public void NewGame()
+        {
+            foreach (var control in gridBoard.Children)
+            {
+                if (control is Button)
+                {
+                    ((Button)control).Content = string.Empty;
+                }
+            }
+            _Logic = new Logic();
+        }
+    }
+
+}
